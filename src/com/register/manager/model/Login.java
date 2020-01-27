13570034e19522370ea9b9336407b435ff494588
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-27 08:22:46 +0100

    created enum login with nick names

---
+++ src/com/register/manager/model/Login.java
@@ -0,0 +1,14 @@
+package com.register.manager.model;
+
+public enum Login {
+    GUFFAW,
+    HITHERTO,
+    LIGATURE,
+    GUIDON,
+    HOARY,
+    LIMNER,
+    GUILLOCHE,
+    HOBBLEDEHOY,
+    TITMOUSE,
+    HOLYSTONE
+}
